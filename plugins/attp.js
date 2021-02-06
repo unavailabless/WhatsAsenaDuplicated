7871ac5b3ac39cb5d13a6905a8875e103c2cbfcf
unavailabless <61870048+unavailabless@users.noreply.github.com>
2021-02-07 00:55:46 +0700

    Update attp.js

--- plugins/attp.js
+++ plugins/attp.js
@@ -15,7 +15,7 @@ Asena.addCommand({pattern: 'attp ?(.*)', fromMe: true, desc: Lang.TTP_DESC}, (as
 
     if (match[1] === '') return await message.sendMessage(Lang.NEED_WORD);
 
-    var ttinullimage = await axios.get(`https://api.xteam.xyz/attp?file&text=${match[1].replace(/Ö/g, "%C3%96").replace(/ö/g, "%C3%B6").replace(/ü/g, "%C3%BC").replace(/Ü/g, "%C3%9C").replace(/Ğ/g, "%C4%9E").replace(/ğ/g, "%C4%9F").replace(/ş/g, "%C5%9F").replace(/Ş/g, "%C5%9E").replace(/ç/g, "%C3%A7").replace(/Ç/g, "%C3%87").replace(/ı/g, "%C4%B1").replace(/i/g, "%69").replace(/"/g, "%22").replace(/İ/g, "%C4%B0")}`, { responseType: 'arraybuffer' })
+    var ttinullimage = await axios.get(`https://api.xteam.xyz/attp?file&text=${match[1]`, { responseType: 'arraybuffer' })
 
     await message.sendMessage(Buffer(ttinullimage.data), MessageType.image, {mimetype: Mimetype.gif, caption: 'Made by WhatsAsena'})
 
